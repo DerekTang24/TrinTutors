322a46e9595ed598e6f147a26d2844b2453044c4
Derek <derektang101@gmail.com>
2024-04-08 23:17:26 -0400

    added footer

---
+++ components/footer.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <footer class="absolute bottom-0 w-full">
+      <p>Made by Derek, Brennan, and Lucas</p>
+    </footer>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,6 @@
   <div>
     <AdminNavbar />
     <slot />
+    <Footer />
   </div>
 </template>
