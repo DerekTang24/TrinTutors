de926120f067e7918b0d5b14925db873b87d57db
BCC1023 <99893234+BCC1023@users.noreply.github.com>
2024-05-07 10:38:55 -0400

    navbar complete

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,37 @@
 <template>
   <div>
-    <AdminNavbar />
+    <template v-if="role_id === 1">
+      <StudentNavbar />
+    </template>
+    <template v-if="role_id === 2">
+      <TutorNavbar />
+    </template>
+    <template v-if="role_id === 3">
+      <TeacherNavbar />
+    </template>
     <slot />
     <Footer />
   </div>
 </template>
+
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+const session = useSupabaseSession();
+let role_id: number | null = null;
+
+if (user.value) {
+  const { data: user_roles, error } = await supabase
+    .from("user_roles")
+    .select("role_id")
+    .eq("user_id", user.value.id);
+
+  if (error) {
+    console.error(error);
+  } else {
+    role_id = user_roles[0].role_id;
+  }
+}
+
+console.log("session", session);
+</script>
