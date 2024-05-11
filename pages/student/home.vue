bfb12c932b391c2a775f1135c22c41b7f2fb2b35
derek <derektang101@gmail.com>
2024-05-11 18:42:07 -0400

    student home working

--- pages/student/home.vue
+++ pages/student/home.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 const supabase = useSupabaseClient();
 const user = useSupabaseUser();
-useHead({ title: "Student Home" });
-
-const tutors = [
-  { id: 1, name: "Michael", subject: "Mathematics" },
-  { id: 2, name: "Jake", subject: "History" },
-  { id: 3, name: "David", subject: "English" },
-];
+const { data: tutorIds, error } = await supabase
+  .from("tutor_student")
+  .select("tutor_id");
+console.log(tutorIds);
+const tutors = await Promise.all(
+  tutorIds.map(async (tutorId) => {
+    const { data: tutors, error } = await supabase
+      .from("tutors")
+      .select("*")
+      .eq("id", tutorId.tutor_id);
+    return tutors[0];
+  }),
+);
 </script>
-
 <template>
   <div class="flex">
     <div class="table-wrp block min-h-96 overflow-x-auto">
@@ -17,18 +22,16 @@ const tutors = [
       <table class="table-auto table-lg table-zebra flex flex-col">
         <thead>
           <tr>
-            <th></th>
             <th>Name</th>
             <th>Subject</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="tutor in tutors" :key="tutor.id">
-            <th>{{ tutor.id }}</th>
+          <tr v-for="tutor in tutors">
             <td>
               <a :href="`/student/work?name=${tutor.name}`">{{ tutor.name }}</a>
             </td>
-            <td>{{ tutor.subject }}</td>
+            <td>{{ tutor.subjects.join(", ") }}</td>
           </tr>
         </tbody>
       </table>
